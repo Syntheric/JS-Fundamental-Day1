c165497a020046d91b4dc2597cc05c5f81820499
Syntheric <MrSyntheric@gmail.com>
2024-01-25 15:28:58 +0700

    Create => [Pengkodisian]

---
+++ Materi/pengkondisian/js/script.js
@@ -0,0 +1,20 @@
+let nilai = 85
+
+if ( nilai > 85 && nilai < 100){
+    console.log("A")
+    }
+if ( nilai > 75 && nilai < 85){
+    console.log("A")
+    }
+if ( nilai > 55 && nilai < 75){
+    console.log("A")
+    }
+if ( nilai > 40 && nilai < 55){
+    console.log("A")
+    }
+if ( nilai > 0 && nilai < 40){
+    console.log("A")
+    }
+else {
+    console.log("Nilai tidak valid, broooo!!!!!!!!!!!!!1111!!!!!")
+}
